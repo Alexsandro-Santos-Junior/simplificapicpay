3ca7004acc4a28f06839fa6c4f866904860ecc49
alexsandro <alexsandro_azuras@hotmail.com>
2024-01-03 16:23:19 -0300

    feat: created Logic

--- src/main/java/com/desafiopicpay/domain/user/User.java
+++ src/main/java/com/desafiopicpay/domain/user/User.java
@@ -1,5 +1,6 @@
 package com.desafiopicpay.domain.user;
 
+import com.desafiopicpay.dtos.UserDTO;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
@@ -39,5 +40,15 @@ public class User {
     // ... esse enum representa um dos valores que passamos na classe `UserType` ...
     @Enumerated(EnumType.STRING)
     private UserType userType;
+
+    public User(UserDTO data){
+        this.firstName = data.firstName();
+        this.lastName = data.lastName();
+        this.balance = data.balance();
+        this.userType = data.userType();
+        this.password = data.password();
+        this.email = data.email();
+
+    }
 
 }
